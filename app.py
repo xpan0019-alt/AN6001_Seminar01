253ed04ab2a81951e7ee898698b65cd717259ac0
xpan0019-alt <xpan0019@gmail.com>
2025-11-21 03:13:00 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,request,render_template
+import joblib
+
 
 app = Flask(__name__)
 
@@ -6,11 +8,30 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+
 @app.route("/main",methods=["GET","POST"])
 def main():
     q=request.form.get('q')
     print(q)
     return(render_template("main.html"))
+
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q=float(request.form.get('q'))
+    print(q)
+    model=joblib.load('dbs.pkl')
+    r = model.predict([[q]])
+
+    return(render_template("dbs_prediction.html",r=r))
+    # return(render_template("dbs_prediction.html",r=r[0][0]))
+
+
 
 if __name__ == "__main__":
     app.run()
